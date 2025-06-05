62a01a86f3365dddd43cb302f0fd3cc59c902664
舰队的偶像-岛风酱! <frg2089@outlook.com>
2025-06-05 22:52:23 +0800

    refactor(github): 重构友链更新工作流
    - 新增 pull 动作，用于创建合并请求
    - 更新 check 动作，增加输出值
    - 重构 update 动作，简化逻辑
    - 优化工作流文件，提高可读性
    
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- .github/actions/check/src/index.ts
+++ .github/actions/check/src/index.ts
@@ -1,8 +1,24 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import { fetch } from 'cross-fetch'
+import * as _gitea from 'gitea-js'
 import * as YAML from 'yaml'
 
-const client = github.getOctokit(core.getInput('token'))
+const client = github.getOctokit(core.getInput('github-token'))
+const gitea = {
+  ..._gitea,
+  context: {
+    server: core.getInput('gitea-server'),
+    token: core.getInput('gitea-token'),
+    owner: core.getInput('gitea-owner'),
+    repo: core.getInput('gitea-repo'),
+  },
+  client: undefined as unknown as _gitea.Api<unknown>,
+}
+gitea.client = gitea.giteaApi(gitea.context.server, {
+  token: gitea.context.token,
+  customFetch: fetch,
+})
 
 const hero = async (action: () => Promise<void>) => {
   try {
@@ -36,6 +52,9 @@ const getIssue = async () => {
 
 await hero(async () => {
   const issue = await getIssue()
+  const branch = `links/gh#${github.context.issue.number}`
+  const origin = `https://github.com/${github.context.issue.owner}/${github.context.issue.repo}/issues/${github.context.issue.number}`
+  const tokenURL = `${gitea.context.server.replace('//', `//${gitea.context.token}@`)}/${gitea.context.owner}/${gitea.context.repo}.git`
 
   const links = Object.fromEntries(
     [
@@ -49,7 +68,31 @@ await hero(async () => {
       .map((i, index) => [`#${github.context.issue.number}:${index}`, i]),
   )
 
+  const msg = `更新友链
+
+${Object.values(links as Record<string, any>)
+  .map(([_, v]) => `- ${v.name.replace('"', '\\"')}`)
+  .join('\r\n')}
+
+详情(${origin})`
+
+  const data = {
+    title: issue.title,
+    body: `${issue.body ?? undefined}
+    
+## Reference
+[原文地址](${origin})
+    `,
+    head: branch,
+    base: 'main',
+  }
+
   console.log(links)
+  console.log(msg)
 
   core.setOutput('links', links)
+  core.setOutput('branch', branch)
+  core.setOutput('token-url', tokenURL)
+  core.setOutput('message', msg)
+  core.setOutput('pr-data', data)
 })
--- .github/actions/update/src/index.ts
+++ .github/actions/update/src/index.ts
@@ -1,28 +1,8 @@
 import * as core from '@actions/core'
-import * as github from '@actions/github'
-import { fetch } from 'cross-fetch'
-import * as _gitea from 'gitea-js'
 import * as fs from 'node:fs'
 
-const target = process.env.TARGET_PATH!
+const target = core.getInput('target')
 const links = JSON.parse(core.getInput('links'))
-
-const gitea = {
-  ..._gitea,
-  context: {
-    server: core.getInput('gitea-server'),
-    token: core.getInput('gitea-token'),
-    owner: core.getInput('gitea-owner'),
-    repo: core.getInput('gitea-repo'),
-  },
-  client: undefined as unknown as _gitea.Api<unknown>,
-}
-gitea.client = gitea.giteaApi(gitea.context.server, {
-  token: gitea.context.token,
-  customFetch: fetch,
-})
-
-const client = github.getOctokit(core.getInput('github-token'))
 
 const hero = async (action: () => Promise<void>) => {
   try {
@@ -33,56 +13,9 @@ const hero = async (action: () => Promise<void>) => {
   }
 }
 
-if (process.argv.includes('--pre')) {
-  await hero(async () => {
-    const branch = `links/gh#${github.context.issue.number}`
-    const origin = `https://github.com/${github.context.issue.owner}/${github.context.issue.repo}/issues/${github.context.issue.number}`
-    const tokenURL = `${gitea.context.server.replace('//', `//${gitea.context.token}@`)}/${gitea.context.owner}/${gitea.context.repo}.git`
-
-    const msg = Object.values(links as Record<string, any>)
-      .map(([_, v]) => `- ${v.name.replace('"', '\\"')}`)
-      .join('\r\n')
-
-    core.setOutput('branch', branch)
-    core.setOutput('token-url', tokenURL)
-    core.setOutput(
-      'message',
-      `更新友链
-
-${msg}
-
-详情(${origin})`,
-    )
-  })
-} else {await hero(async () => {
-  const branch = `links/gh#${github.context.issue.number}`
-
+await hero(async () => {
   const text = await fs.promises.readFile(target, 'utf-8')
   const allLinks = JSON.parse(text)
   const newLinks = Object.assign(allLinks, links)
   await fs.promises.writeFile(target, JSON.stringify(newLinks, null, 2))
-
-  const res = await client.rest.issues.get({
-    issue_number: github.context.issue.number,
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-  })
-  if (res.status !== 200) core.setFailed(`Failed to get issue: ${res.status}`)
-  const issue = res.data
-
-  gitea.client.repos.repoCreatePullRequest(
-    gitea.context.owner,
-    gitea.context.repo,
-    {
-      title: issue.title,
-      body: `${issue.body ?? undefined}
-      
-## Reference
-[原文地址](${origin})
-      `,
-      head: branch,
-      base: 'main',
-    },
-  )
 })
-}
